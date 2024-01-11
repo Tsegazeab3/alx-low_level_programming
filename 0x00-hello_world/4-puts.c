98387017d2fac3197f47fa1618f55cbbf7dd2f01
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-11 08:01:35 +0400

    getting back to alx part 2

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints 'programming is like building a multilingual puzzle'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle");
+	return (0);
+}
