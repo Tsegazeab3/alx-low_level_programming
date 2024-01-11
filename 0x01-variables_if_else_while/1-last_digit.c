bb6c9ad715139b740e418dea8dae40984151899e
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-12 01:29:58 +0400

    getting my mistake was a space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,24 +8,15 @@
 int main(void)
 {
 	int n;
-	int k;
 	int rem;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >= 0)
-	{
-		k = n;
-	}
-	else
-	{
-		k = -n;
-	}
-	rem = k % 10;
+	rem = n % 10;
 
 	if (rem > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5 \n", n, rem);
+		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	}
 	else if (rem == 0)
 	{
