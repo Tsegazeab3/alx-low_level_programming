62b3a3154fe51359b51a18e1640405da27095658
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-12 15:15:09 +0400

    understood the main thing about functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - prints _putchar
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar");
+	char letter[] = "_putchar";
+	int k = 0;
+
+	while (k <= 8)
+	{
+		_putchar(letter[k]);
+		k++;
+	}
+	_putchar('\n');
 	return (0);
 }
