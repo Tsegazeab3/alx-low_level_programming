ed152020648aba60f226d03e845180731d0f327c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-16 14:43:53 +0400

    that's number 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int k = '0';
+
+	while (k >= '0' and k <= '9')
+	{
+		_putchar(k);
+		_putchar('\n');
+		k++;
+	}
+}
