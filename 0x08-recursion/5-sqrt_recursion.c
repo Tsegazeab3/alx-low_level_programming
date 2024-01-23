91c40b29f1934f67afb272d901cca9489b7c09c7
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-23 14:35:24 +0400

    there might be something wrong with my computer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,45 @@
 #include "main.h"
 /**
+ * _sqrt_aider - aids the above function
+ * @n: number to check with
+ * @low: the lowest number
+ * @high: the highest num
+ * Return: depends on the number
+ */
+int _sqrt_aider(int n, int low, int high);
+/**
  * _sqrt_recursion - returns the natural sqare root of a number
  * @n: number to be square rooted
- * Return: integer 
+ * Return: integer
  */
-int _sqrt_aid(int x, low, high);
 int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
-		return (n);
 	if (n < 0)
 		return (-1);
-	else:
-		int low = 0;
-		int mid = n / 2
-		_sqrt_aid(n, low, mid);
+	else if (n == 1 || n == 0)
+		return (n);
+	else
+		return (_sqrt_aider(n, 0, n));
 }
-int _sqrt_aid(high, low, mid)
+/**
+ * _sqrt_aider - aids the above function
+ * @n: number to check with
+ * @low: the lowest number
+ * @high: the highest num
+ * Return: depends on the number
+ */
+int _sqrt_aider(int n, int low, int high)
 {
-	mid = (low + high) / 2;
-	if (mid * midd > high)
-		_sqrt_aid (high, low, mid - 1);
-     	else if ( mid * mid  < x)
+	int mid = (high + low) / 2;
+
+	if (mid * mid < n && (mid + 1) * (mid + 1) > n)
+		return (-1);
+	else if (mid * mid == n)
+		return (mid);
+	else if (mid * mid < n)
+		return (_sqrt_aider(n, mid + 1, high));
+	else if (mid * mid > n)
+		return (_sqrt_aider(n, low, mid));
+	else
+		return (mid);
+}
