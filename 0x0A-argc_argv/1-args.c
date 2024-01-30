a49591cf980e496dc57f99306f757d8d9818e77b
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-30 23:16:01 +0400

    did i get this right?

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - prints the number of arguments in a command line
+ * @argc: the number of argumenst in the command line
+ * @argv: the array of arguments
+ * Return: 0 if success
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d \n", argc - 1);
+	return (0);
+}
