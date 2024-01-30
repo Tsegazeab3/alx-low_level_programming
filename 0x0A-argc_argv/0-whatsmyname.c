caf0ebb3aa19f100729b732f1bdc08ead601a21a
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-30 23:04:26 +0400

    i fixed it and it was easy

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,14 +7,15 @@
  * @argv: an array consisting of the comman line articles
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i;
 	int j = 0;
+
 	while (argv[0][j])
 	{
 		j++;
-		printf("%c",argv[0][j]);
+		printf("%c", argv[0][j]);
 	}
 	printf("\n");
+	return (0);
 }
