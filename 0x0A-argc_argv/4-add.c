fe4e34198b3e26292aabe974d396a9c5f16e5538
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-31 10:17:49 +0400

    question number 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * isInteger - checks whether a string is an integer only
+ * @str: string to be checked
+ * Return: boolean depending on the result
+ */
+int isInteger(const char *str)
+{
+	char *endptr;
+
+	strtol(str, &endptr, 10);
+	return (*endptr == '\0');
+}
+/**
+ * main - finds the sum of arguments in the command line
+ * @argc: the number of argunments in the command line
+ * @argv: the array of argunments in the command line
+ * Return: 0 if successful 1 if not
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	int i = 1;
+	int j = 0;
+
+	if (argc == 1)
+		return (0);
+	while (argv[i])
+	{
+		if (isInteger(argv[i]))
+			j += atoi(argv[i]);
+		else
+		{
+			printf("Error");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d", j);
+	printf("\n");
+	return (0);
+}
