fea7768d0574edfd6af47596ea2d27e3db281e7c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-02-08 14:27:19 +0400

    i am sure i missed something

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strdup - copies an array
+ * @str: string to be copied
+ * Return: the location of the first string
+ */
+char *_strdup(char *str)
+{
+	if (str != NULL)
+	{
+		int i = strlen(str);
+		char *p = (char *)malloc((i + 1) * sizeof(char));
+		int j = 0;
+
+		while (j < i)
+		{
+			p[j] = str[j];
+			j++;
+		}
+		p[j] = '\0';
+		return (p);
+	}
+	else
+		return (NULL);
+}
