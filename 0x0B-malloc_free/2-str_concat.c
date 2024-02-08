1982cc5a57d5da8ad0242743b457fa50d4fe7e2c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-02-08 14:56:21 +0400

    i fixed something

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: final string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p = (char *)calloc((strlen(s1) + strlen(s2) + 1), sizeof(char));
+	int i = 0;
+	int j = 0;
+
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+	p[i + 1] = '\0';
+	return (p);
+}
