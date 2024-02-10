aff4d047dba81a10c1e8eda507c64181d9e3f386
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-02-10 13:02:34 +0400

    i don't what is different from the past

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * alloc_grid - creates 2d lists
+ * @width: width of the arrays
+ * @height: height of the arrays
+ * Return: pointer to a double list
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = (int **)malloc(height * sizeof(int *));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *) malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(p[i]);
+			}
+			free(p);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
