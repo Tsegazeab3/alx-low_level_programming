b6952db44291d134665afe3223408e6d86ea0e15
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-03 19:16:49 +0400

    i fixed one error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * struct dog - describes characterstics of a dog
  * @name: string of a name
  * @age: age of the dog
  * @owner: the name of the owner
 */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
-typedef dog dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
