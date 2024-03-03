bcf888f53181bc2689297be214b0cd7488b96625
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-03 21:18:15 +0400

    checking question no 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,10 +5,14 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d = NULL)
-		d = malloc(sizeof(dog));
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		printf("(nil)");
+	else if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 }
-
