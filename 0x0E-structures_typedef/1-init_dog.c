01b5769debed936cf3cd461d71da40e9bb546740
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-03 19:03:55 +0400

    checking something for my self '

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,6 +8,8 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	strcpy(d->name, name);
 	d->age = age;
 	strcpy(d->owner, owner);
