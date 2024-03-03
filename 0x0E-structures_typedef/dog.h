26d08104a0536dc52dbd079b0021f1fd0815ce8f
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-03 21:29:43 +0400

    added the stdio.h library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 /**
@@ -15,6 +16,9 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+*/
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
