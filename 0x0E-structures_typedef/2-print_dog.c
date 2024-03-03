7ca7de8f7143b5c317247436f907559098ba37f4
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-03 21:36:07 +0400

    it said the element not the entire pointer

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,13 +6,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("(nil)");
-	else if (d->name == NULL)
-		printf("Name: (nil)");
-	else
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
-	}
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
