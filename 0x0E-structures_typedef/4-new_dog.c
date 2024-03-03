2879f61fc25141046f010a28ade7e025bd32129c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-03 19:54:10 +0400

    i checking question no 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,13 +4,23 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: name of the owner of the dog
+ * Return: a pointer to the new dog
 */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (new_dog == NULL)
-		new_dog = malloc(sizeof(dog_t));
-	strcpy(new_dog -> name, name);
-	new_dog -> age = age;
-	strcpy(new -> owner, owner);
-	return (new_dog);
+	dog_t new_dog;
+	dog_t *dog = &new_dog;
+	int len1 = strlen(name + 1);
+	int len2 = strlen(owner + 1);
+
+	new_dog.name = malloc(len1);
+	if (new_dog.name == NULL)
+		return (NULL);
+	new_dog.owner = malloc(len2);
+	if (new_dog.name == NULL)
+		return (NULL);
+	strcpy(new_dog.name, name);
+	new_dog.age = age;
+	strcpy(new_dog.owner, owner);
+	return (dog);
 }
