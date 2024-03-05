358fa69716e913916c474bda5f32cd349174b68d
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-05 10:59:43 +0400

    the porblem was with the header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 void print_name(char *name, void (*f)(char *));
-oid array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
