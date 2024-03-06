3b2ae0edfd80d5a38ddc4e070fa71929b29057a3
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-06 15:25:03 +0400

    i fixed my answers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,23 +3,25 @@
  * main - simple calculator
  * @argc: the number of argunments
  * @argv: the array of argunments
+ * Return: integers results of the argunments
 */
 int main(int argc, char *argv[])
 {
 	if (argc == 4)
 	{
 		int (*func)(int, int) = get_op_func(argv[2]);
+		int a;
 
-		if (func)
+		if (!func)
 		{
-			int a = func(atoi(argv[1]), atoi(argv[3]));
-
-			printf("%d\n", a);
-			return (EXIT_SUCCESS);
+			printf("ERROR\n");
+			exit(99);
 		}
-		printf("ERROR\n");
-		exit (99);
+		a = func(atoi(argv[1]), atoi(argv[3]));
+
+		printf("%d\n", a);
+		return (EXIT_SUCCESS);
 	}
 	printf("ERROR\n");
-	exit (98);
+	exit(98);
 }
