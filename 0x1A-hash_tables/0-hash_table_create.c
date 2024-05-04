6775c2e9088085664740c304f082061af70e1366
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-04 16:44:54 +0400

    trying to free everythin

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,14 +16,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		Table->size = size;
 		Table->array = malloc(sizeof(hash_node_t *) * size);
 		if (Table->array == NULL)
+		{
+			free(Table);
 			return (NULL);
+		}
 		for (i = 0; i < size; i++)
 			Table->array[i] = NULL;
 
-		if (Table != NULL)
-		{
-			return (Table);
-		}
+		return (Table);
 	}
 	return (NULL);
 }
