7773ae6009de81ca4133a1b5bf00cc2187c30b0c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-04 16:20:24 +0400

    did something to include everyone

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,11 +6,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *Table = malloc(size * sizeof(hash_node_t)); 
-    if (Table != NULL) 
-    {
-        return(Table);
-    }
-    return(NULL);
+	if (size > 0)
+	{
+		hash_table_t *Table = malloc(size * sizeof(hash_node_t));
+
+		if (Table != NULL)
+		{
+			return (Table);
+		}
+	}
+	return (NULL);
 }
 
