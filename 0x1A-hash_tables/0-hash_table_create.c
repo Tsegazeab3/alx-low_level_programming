d3c4eb463363841e9eb447bff1a7c8a15b18a264
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-04 05:12:05 +0400

    git check

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates hash tables
+ * @size: the size of the table
+ * Return: a pointer to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *Table = malloc(size * sizeof(hash_node_t)); 
+    if (Table != NULL) 
+    {
+        return(Table);
+    }
+    return(NULL);
+}
+
