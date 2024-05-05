b7e4655a0355c88becb78377a63f5a85097e3902
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-05 22:51:49 +0400

    checking somethingg

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,46 +1,43 @@
 #include "hash_tables.h"
 #include <string.h>
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hast_table
- * @key: the nodes key word
- * @value: the value of the key
- * Return: 1 if success 0 otherwise
+ * hash_table_set - sets a node into a table
+ * @ht: the table
+ * @key: the key of the node
+ * @value: the value represented by the key
+ * Return: 0 if not success and 1 if success
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *key_cp = malloc(sizeof(key));
-	char *value_cp = malloc(sizeof(value));
-	hash_node_t *node = malloc(sizeof(hash_node_t *));
-	unsigned long int index;
+	char *key_copy;
+	hash_node_t *node;
+	int index;
 
-	strcpy(key_cp, key);
-	strcpy(value_cp, value);
-	if (key_cp == NULL || value_cp == NULL || ht == NULL)
+	key_copy = malloc(strlen(key) + 1);
+	if (key_copy == NULL || value == NULL || ht == NULL)
 	{
-		free(key_cp);
-		free(value_cp);
-		free(node);
+		free(key_copy);
 		return (0);
 	}
-
-	node->key = key_cp;
-	node->value = value_cp;
+	strcpy(key_copy, key);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = key_copy;
+	node->value = (char *)value;
 	node->next = NULL;
-	index = key_index((const unsigned char *)node->key, ht->size);
+	index = key_index((const unsigned char *)key_copy, ht->size);
 	if (ht->array[index] != NULL)
 	{
-		hash_node_t *tmp = malloc(sizeof(hash_node_t *));
-
-		tmp = ht->array[index];
+		node->next = ht->array[index];
 		ht->array[index] = node;
-		node->next = tmp;
 		return (1);
 	}
-	else
+	else if (ht->array[index] == NULL)
 	{
 		ht->array[index] = node;
 		return (1);
 	}
 	return (0);
 }
+
