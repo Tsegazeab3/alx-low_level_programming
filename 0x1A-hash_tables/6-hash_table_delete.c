62d81b9f6218fcf9474e2cfe185f812408f1e63c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-06 14:16:20 +0400

    finished this lesson for the most part

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes the table
+ * @ht: the table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht != NULL)
+	{
+
+		hash_node_t *next;
+		hash_node_t *current;
+		unsigned long int i;
+
+		for (i = 0; i < ht->size; i++)
+		{
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				next = current->next;
+				free(current->key);
+				free(current->value);
+				free(current->next);
+				free(current);
+				current = next;
+			}
+			ht->array[i] = NULL;
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
