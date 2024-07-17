a9aecc73ffc37829de7d39738bf98dbf5eea44f1
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-07-17 16:23:34 +0400

    i didn't commit everything i was at the wrong file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,6 @@
 #ifndef SEARCH_ALGO_H
 #define SEARCH_ALGO_H
+#include <stdio.h>
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
