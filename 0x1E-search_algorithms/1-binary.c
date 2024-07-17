17b7582dc0501c69b71be6e6be852109a3640665
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-07-17 22:25:09 +0400

    i think i get right this time

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,32 +26,39 @@ void print_array(int *array, int size)
 
 	printf("\n");
 }
+
 /**
-* binary_search - searches through a sorted array in the binary_algorithm
-* @array: the array to be searched
-* @size: size of the array
-* @value: value to be searched
-* Return: the index of the value
-*/
+ * binary_search - find value in sorted array using binary search method
+ * @array: pointer to first element in array to be searched
+ * @size: size of the array (number of elements)
+ * @value: value to be searched for
+ *
+ * Return: index position of value or -1 if not found or array is null
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0;
-	size_t high = size - 1;
+	int low = 0;
+	int high = size - 1;
 	int mid;
 
-	if (array != NULL)
+	if (array == NULL || size == 0 || array[0] > value)
+		return (-1);
+
+	while (low <= high)
 	{
-		while (low < high)
-		{
-			print_array(&array[low], (high - low) + 1);
-			mid = (low + high) / 2;
-			if (array[mid] == value)
-				return (mid);
-			else if (array[mid] < value)
-				low = mid + 1;
-			else if (array[mid] > value)
-				high = mid - 1;
-		}
+		mid = (low + high) / 2;
+
+		print_array(&array[low], (high - low) + 1);
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] > value)
+			high = mid - 1;
+
+		else
+			low = mid + 1;
 	}
+
 	return (-1);
 }
