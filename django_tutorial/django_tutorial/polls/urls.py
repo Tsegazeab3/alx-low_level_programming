e1bfcedc5e00870e6fa780f6761478d06dd5c477
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2025-04-18 21:31:14 +0400

    changin somethings

--- django_tutorial/django_tutorial/polls/urls.py
+++ django_tutorial/django_tutorial/polls/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-        path('', views.index, name = "index"),
+    path('polls', views.index, name = "index"),
+    path("/polls/question_number/<int:question_id>/", views.detail,name="detail"),
+    path("/polls/question_number/<int:question_id>/results", views.results, name= "results"),
+    path("</polls/question_number/<int:question_id>/vote", views.vote, name="votes"), 
+
+
 ]
--- django_tutorial/django_tutorial/polls/views.py
+++ django_tutorial/django_tutorial/polls/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse, response 
+def index(request):
+    return HttpResponse("welcome to polls app".encode('utf-8'))
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {str(question_id)}")
+    return HttpResponse(f"You're looking at question {str(question_id)}".encode('utf-8'))
+def results(request, question_id):
+    response = f"You're looking at question{str(question_id)}."
+    return(HttpResponse(response.encode('utf-8')))
+def vote(request, question_id):
+         return HttpResponse(f"You're voting on question {question_id}.".encode('utf-8'))
+
 
