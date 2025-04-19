ab9e276584e012eae39481c867c460bc90c193ee
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2025-04-19 11:11:13 +0400

    commiting yesterdays changes

--- django_tutorial/django_tutorial/polls/urls.py
+++ django_tutorial/django_tutorial/polls/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns= [
     path('polls', views.index, name = "index"),
-    path("/polls/question_number/<int:question_id>/", views.detail,name="detail"),
-    path("/polls/question_number/<int:question_id>/results", views.results, name= "results"),
-    path("</polls/question_number/<int:question_id>/vote", views.vote, name="votes"), 
+    path("polls/question_number/<int:question_id>/", views.detail,name="detail"),
+    path("polls/question_number/<int:question_id>/results", views.results, name= "results"),
+    path("polls/question_number/<int:question_id>/vote", views.vote, name="votes"), 
 
 
 ]
--- django_tutorial/django_tutorial/polls/views.py
+++ django_tutorial/django_tutorial/polls/views.py
@@ -1,12 +1,21 @@
-from django.http import HttpResponse, response 
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Question
+def return_question_text(question):
+    return(question.question_text)
 def index(request):
-    return HttpResponse("welcome to polls app".encode('utf-8'))
+    q = Question.objects.order_by("pub_date")[:5]
+    output=", ".join(map(return_question_text, q))
+    context= {"q": q}
+    return render(request,"polls/index.html", context)
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {str(question_id)}".encode('utf-8'))
+    try:
+        question = Question.objects.get(pk=question_id)
+        return(render(request, "polls/detail.html",{"question": question}))
+    except Http404:
+        return(render(request, "polls/404.html",{"question_id": question_id}))
 def results(request, question_id):
     response = f"You're looking at question{str(question_id)}."
     return(HttpResponse(response.encode('utf-8')))
 def vote(request, question_id):
          return HttpResponse(f"You're voting on question {question_id}.".encode('utf-8'))
-
-
